5fcb5f1766cefc10c999004d40b3b0f56bb33654
tal-nagra <tal@initialspark.co.uk>
2024-06-26 21:35:59 +0100

    RPS-88: Update the iteration zero journey for the show & tell

--- app/routes/payment.js
+++ app/routes/payment.js
@@ -4,14 +4,13 @@ const viewTemplate = 'payment'
 const currentPath = `${urlPrefix}/${viewTemplate}`
 const ACTION_YAR_KEY = 'selectedActions'
 const RAW_ACTION_YAR_KEY = 'rawActions'
-const DF_SBI = 200599768
 
-const createModel = (paymentAmount, action, actionQuantity) => {
+const createModel = (paymentAmount, action, actionQuantity, sbi) => {
   return {
     paymentAmount: paymentAmount.toFixed(2),
     action,
     actionQuantity,
-    sbi: DF_SBI
+    sbi
   }
 }
 
@@ -35,10 +34,11 @@ module.exports = [
     handler: async (request, h) => {
       const selectedActions = getYarValue(request, ACTION_YAR_KEY) ?? []
       const rawActions = getYarValue(request, RAW_ACTION_YAR_KEY) ?? []
-      const action = rawActions[0].code + ':' + rawActions[0].description
+      const sbi = getYarValue(request, 'chosen-organisation')
+      const action = rawActions[0].code + ': ' + rawActions[0].description
       const paymentAmount = await getPaymentAmount(selectedActions)
       const actionQuantity = selectedActions[0].quantity
-      return h.view(viewTemplate, createModel(paymentAmount, action, actionQuantity))
+      return h.view(viewTemplate, createModel(paymentAmount, action, actionQuantity, sbi))
     }
   }
 ]
