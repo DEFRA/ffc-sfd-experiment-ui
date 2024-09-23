2e68c3cc769b230b4c6c54ea1d4689e18dc8a855
Mayank Solanki <manky18@gmail.com>
2024-09-23 15:55:38 +0100

    rps-215 adding acceptance test for the grh1 action

--- test/acceptance/features/pageobjects/future-rps-payment-calculation-page.js
+++ test/acceptance/features/pageobjects/future-rps-payment-calculation-page.js
@@ -1,10 +1,15 @@
 const { $ } = require('@wdio/globals')
 const CommonActions = require('./common-actions');
 
+const ACTION_NAME = "tbody tr:nth-child(3) td:nth-child(1)"
 const LAND_AREA = "tbody tr:nth-child(3) td:nth-child(2)"
 const PAYMENT_AMOUNT = "tbody tr:nth-child(3) td:nth-child(3)"
 
 class FutureRpsPaymentCalculationPage extends CommonActions {
+
+    async validateActionName (action) {
+        await this.elementToContainText(ACTION_NAME, action)
+    }
 
     async validateLandArea (area) {
         await this.elementToContainText(LAND_AREA, area)
--- test/acceptance/features/step-definitions/farmer-journey-steps.js
+++ test/acceptance/features/step-definitions/farmer-journey-steps.js
@@ -3,10 +3,14 @@ const LoginPage = require('../pageobjects/future-rps-login-page');
 const EligibilityPage = require('../pageobjects/future-rps-eligibility-evaluation-page');
 const LandActionsPage = require('../pageobjects/future-rps-land-actions-page');
 const PaymentsPage = require('../pageobjects/future-rps-payment-calculation-page');
+const commonAPICalls = require('../pageobjects/common-api-calls');
+const { waitFor } = require('@babel/core/lib/gensync-utils/async')
 
 const pages = {
     login: LoginPage
 }
+
+let available_area_for_land_action = 0
 
 Given(/^(.*) is on the Rural Payments Service (\w+) page$/, async function (farmer, page) {
     await pages[page].open()
@@ -28,33 +32,27 @@ When(/^(.*) is eligible to apply for funding$/, async function (farmer) {
     await EligibilityPage.clickContinueButton()
 });
 
-When(/^(.*) selects the land parcel type of Arable land$/, async function (farmer) {
-    await LandActionsPage.selectArableLandParcel()
+When(/^(.*) selects the land parcel type of (.*)$/, async function (farmer, landType) {
+    await LandActionsPage.selectLandParcel(landType)
 });
-Then(/^she can choose to apply for SAM1 and or SAM, but not LIG1$/, async function () {
-    await LandActionsPage.selectSam1Action()
-    await LandActionsPage.enterQuantityForSAM1('8.4')
+
+When(/^the (.*) has the (.*) and (.*) hectare requirement$/, async function (action, min, max) {
+    await commonAPICalls.updateMinParcelArea(action, min)
+    await commonAPICalls.updateMaxParcelArea(action, max)
 });
-Then(/^Sarah is shown amount she will receive for Arable land$/, async function () {
-    await PaymentsPage.validateLandArea(8.4)
-    await PaymentsPage.validatePaymentAmount(48.72)
+
+When(/^(\w+) chooses to apply for (.*) for (.*) hectares$/, async function (farmer, action, available_area) {
+    available_area_for_land_action = available_area
+    await LandActionsPage.selectAction(action)
+    await LandActionsPage.enterQuantityForAction(action,available_area_for_land_action)
 });
-When(/^Sarah selects the land parcel type of Permanent Grassland$/, async function () {
-    await LandActionsPage.selectPermanentGrasslandParcel()
+
+Then(/^(.*) is shown (.*) she will receive for this (.*)$/, async function (farmer, payment_amount, action) {
+    await PaymentsPage.validateActionName(action)
+    await PaymentsPage.validateLandArea(available_area_for_land_action)
+    await PaymentsPage.validatePaymentAmount(payment_amount)
 });
-Then(/^she can choose to apply for SAM1 and or LIG1, but not SAM2$/, async function () {
-    await LandActionsPage.selectSam1Action()
-    await LandActionsPage.enterQuantityForSAM1('4.2')
-});
-Then(/^Sarah is shown amount she will receive for Permanent Grassland$/, async function () {
-    await PaymentsPage.validateLandArea(4.2)
-    await PaymentsPage.validatePaymentAmount(24.36)
-});
-Then(/^he can choose to apply for SAM1, but not SAM2, SAM3 or LIG1$/, async function () {
-    await LandActionsPage.selectSam1Action()
-    await LandActionsPage.enterQuantityForSAM1('20')
-});
-Then(/^Jim is shown amount she will receive for Arable land$/, async function () {
-    await PaymentsPage.validateLandArea(20)
-    await PaymentsPage.validatePaymentAmount(116.00)
+
+Then(/^Sarah is shown the error message that the area is below the minimum requirement$/, async function () {
+    await LandActionsPage.validateErrorMessage('GRH1: The parcel must have a total area of at least 15ha')
 });
