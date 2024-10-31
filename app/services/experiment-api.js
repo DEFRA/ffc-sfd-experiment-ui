75b092f88dc0a57334a6478a617d0110c7e32e18
John Bower <john@industrialassets.co.uk>
2024-10-31 14:08:24 +0000

    fixed some issues to make it compatible with the new backend

--- app/services/experiment-api.js
+++ app/services/experiment-api.js
@@ -1,73 +1,84 @@
-const { experimentApiBaseUrl } = require('../config/server')
+const { experimentApiBaseUrl } = require("../config/server");
 
 const invokeGetEndpoint = async (endpoint, defaultReturnValue) => {
   // eslint-disable-next-line no-undef
-  const response = await fetch(`${experimentApiBaseUrl}/${endpoint}`)
-  const deserialisedResponseBody = await response.json()
-  return deserialisedResponseBody ?? defaultReturnValue
-}
+  const response = await fetch(`${experimentApiBaseUrl}/${endpoint}`);
+  const deserialisedResponseBody = await response.json();
+  return deserialisedResponseBody ?? defaultReturnValue;
+};
 
 const invokePostEndpoint = async (endpoint, requestPayload) => {
   // eslint-disable-next-line no-undef
-  const response = await fetch(
-        `${experimentApiBaseUrl}/${endpoint}`,
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(requestPayload)
-        })
-  const deserializedResponse = await response.json()
-  return deserializedResponse ?? null
-}
+  const response = await fetch(`${experimentApiBaseUrl}/${endpoint}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestPayload),
+  });
+  const deserializedResponse = await response.json();
+  return deserializedResponse ?? null;
+};
 
 const validateActions = async (actions, landParcel) => {
-  return invokePostEndpoint('action-validation', { actions, landParcel })
-}
+  return invokePostEndpoint("action-validation", { actions, landParcel });
+};
 
 const getLandParcels = async (sbi) => {
-  return invokeGetEndpoint(`land-parcel/${sbi}`, [])
-}
+  return invokeGetEndpoint(`land/parcel/${sbi}`, []);
+};
 
-const getActions = async (selectedLandParcelId, landUseCodes, preexistingActions) => {
-  const landUseCodesString = landUseCodes.join(',')
-  return invokeGetEndpoint(`action?parcel-id=${selectedLandParcelId}&land-use-codes=${encodeURIComponent(landUseCodesString)}&preexisting-actions=${preexistingActions}`, [])
-}
+const getActions = async (
+  selectedLandParcelId,
+  landUseCodes,
+  preexistingActions
+) => {
+  const landUseCodesString = landUseCodes.join(",");
+  return invokeGetEndpoint(
+    `action?parcel-id=${selectedLandParcelId}&land-use-codes=${encodeURIComponent(
+      landUseCodesString
+    )}&preexisting-actions=${preexistingActions}`,
+    []
+  );
+};
 
-const calculateAvailableArea = async (actionCode, selectedLandParcel, landUseCodes) => {
-  return invokePostEndpoint('available-area', {
+const calculateAvailableArea = async (
+  actionCode,
+  selectedLandParcel,
+  landUseCodes
+) => {
+  return invokePostEndpoint("available-area", {
     applicationFor: actionCode,
     landParcel: {
       area: selectedLandParcel.area,
       existingAgreements: selectedLandParcel.agreements?.length
         ? selectedLandParcel.agreements.map((agreement) => {
-            return { area: agreement.area, code: agreement.actionCode }
+            return { area: agreement.area, code: agreement.actionCode };
           })
-        : []
+        : [],
     },
-    landUseCodes
-  })
-}
+    landUseCodes,
+  });
+};
 
 const calculatePaymentAmount = async (selectedActions, landUseCodes) => {
   if (selectedActions?.length) {
     const payload = {
-      actions: selectedActions.map(action => ({
-        'action-code': action.actionCode,
-        'hectares-applied-for': parseFloat(action.quantity)
+      actions: selectedActions.map((action) => ({
+        "action-code": action.actionCode,
+        "hectares-applied-for": parseFloat(action.quantity),
       })),
-      'land-use-codes': landUseCodes
-    }
-    return invokePostEndpoint('payment-calculation', payload)
+      "land-use-codes": landUseCodes,
+    };
+    return invokePostEndpoint("payment-calculation", payload);
   }
-  return []
-}
+  return [];
+};
 
 const submitFundingApplication = async (fundingApplication) => {
-  return invokePostEndpoint('funding-application', fundingApplication)
-}
+  return invokePostEndpoint("funding-application", fundingApplication);
+};
 
 module.exports = {
   getLandParcels,
@@ -75,5 +86,5 @@ module.exports = {
   calculateAvailableArea,
   calculatePaymentAmount,
   submitFundingApplication,
-  validateActions
-}
+  validateActions,
+};
